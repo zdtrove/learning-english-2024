94f93982137306f52d120bd176bbfb3b30e927c8
Hoang An <antnh@icd-vn.com>
2024-11-01 15:38:08 +0700

    Commit update english

--- js/script.js
+++ js/script.js
@@ -50,7 +50,7 @@ function getSection(num, id) {
 
 function loadAllContents() {
   getSection(1, 'conversation');
-  getSection(5, 'english-speaking-course');
+  getSection(10, 'english-speaking-course');
   getSection(2, 'easy-english');
   // getSection(7, 'ielts-speaking');
   // getSection(1, 'ielts-listening');
