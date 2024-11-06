6025a01f6be2bbeb768bddad30edff7bd95be2b7
Hoang An <antnh@icd-vn.com>
2024-11-06 13:21:45 +0700

    Commit update english

--- js/mp3-list.js
+++ js/mp3-list.js
@@ -382,4 +382,5 @@ const englishSpeakingCourseMp3 = {
   '010': 'ptvqtivd1kk24yh8lqk1x/010.mp3?rlkey=nepwcmtoynizg16f7gsouqy88',
   '011': 'fldz9gltu2a9cxbgbp6wy/011.mp3?rlkey=ar9pibgv710l45e9urlir88dd',
   '012': 'p8ohyg2xhei48bkhimxuo/012.mp3?rlkey=9jyqhzafi3o1pedvzsb9sapk0',
+  '013': '00vn11pgt7jrn6ljp0ipg/013.mp3?rlkey=oz1tb05a63f5orbdvoy72ogjo',
 }
